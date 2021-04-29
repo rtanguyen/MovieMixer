88502b590ab7af5a4999e19b8ae2d66b55b8be2f
Mimi Vo <mimzy414@gmail.com>
2021-04-29 15:55:12 -0500

    start of drink pseudo

---
+++ assets/js/drinksscript.js
@@ -0,0 +1,27 @@
+// MOVIE MIXER LOGIC
+//====================== VARIABLES ======================//
+
+
+
+//====================== RANDOM DRINK ======================//
+    //WHEN THE PAGE LOADS
+        USER PRESENTED WITH AN ALCOHOL OPTION DROPDOWN 
+        USER WILL SELECT ONE OF THE CHOICES IN THE LIST
+
+
+    // WHEN AN ALCOHOL OPTION IS SELECTED
+    function randomDrink() {}
+        A DRINK OPTION WILL DISPLAY
+        RECIPE WILL DISPLAY
+            - IF THE USER IS SATISFIED WITH THE DRINK CHOICE 
+                - DO NOTHING 
+
+            - IF THE USER IS NOT SATISFIED WITH THE DRINK CHOICE
+                - WHEN USER CLICKS THE TRY AGAIN BUTTON, A NEW OPTION WILL BE DISPLAYED 
+
+    // WHEN USER SELECTS A NEW ALCOHOL OPTION
+        RUN THE randomDrink FUNCTION AGAIN
+
+
+//====================== LOGIC AND RUN ======================//
+
