78e4c040a854f3054ac87199a4893e8d617fa147
Mimi Vo <mimzy414@gmail.com>
2021-05-01 12:17:43 -0500

    reorder randomDrink call

--- assets/js/drinksscript.js
+++ assets/js/drinksscript.js
@@ -1,26 +1,4 @@
 // MOVIE MIXER LOGIC
-//====================== VARIABLES ======================//
-
-
-
-//====================== RANDOM DRINK ======================//
-    //WHEN THE PAGE LOADS
-        USER PRESENTED WITH AN ALCOHOL OPTION DROPDOWN 
-        USER WILL SELECT ONE OF THE CHOICES IN THE LIST
-
-
-    // WHEN AN ALCOHOL OPTION IS SELECTED
-    function randomDrink() {}
-        A DRINK OPTION WILL DISPLAY
-        RECIPE WILL DISPLAY
-            - IF THE USER IS SATISFIED WITH THE DRINK CHOICE 
-                - DO NOTHING 
-
-            - IF THE USER IS NOT SATISFIED WITH THE DRINK CHOICE
-                - WHEN USER CLICKS THE TRY AGAIN BUTTON, A NEW OPTION WILL BE DISPLAYED 
-
-    // WHEN USER SELECTS A NEW ALCOHOL OPTION
-        RUN THE randomDrink FUNCTION AGAIN
 
 
 //====================== LOGIC AND RUN ======================//
@@ -34,7 +12,7 @@
 
 //placeholder value
 var userSelection = "vodka"; 
-randomDrink(userSelection);
+var drinkResultEl = document.querySelector('#drink-display')
 
 //pick cocktail based on user choice, grab drink ID
 var randomDrink = function (selection) {
@@ -60,6 +38,8 @@ var randomDrink = function (selection) {
 			console.error(err);
 		});
 };
+
+randomDrink(userSelection);
 
 //pull detail based on drink ID
 var getDrinkInstr = function (id) {
