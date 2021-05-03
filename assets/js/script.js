6168dca4db2865035ba6eb22175ece33539361cc
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-05-03 13:48:03 -0500

    accepted merge change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-// MOBILE STYLING
+// MOBILE LOGIC
 
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links')
 
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
+});
+
+// MODAL LOGIC
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+console.log(signupButton.innerHTML)
+
+signupButton.addEventListener('click', () =>{
+    console.log('test');
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
 });
