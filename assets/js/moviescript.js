413b09e63f61ba024ea8cb07b7f700ef8338fa13
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-09 10:00:32 -0500

    commented out load function

--- assets/js/moviescript.js
+++ assets/js/moviescript.js
@@ -97,7 +97,7 @@ function toggleFavorite(favorite) {
   }
   localStorage.setItem("savedMovies", JSON.stringify(favoriteArray));
 }
-//save favorite
+//save favorite to favoriteMovieArr
 function savedFavoriteMovie(movie) {
    if (favoriteEl.classList.contains("fas")) {
     favoriteMovieArr.push(movie);
@@ -109,28 +109,29 @@ function savedFavoriteMovie(movie) {
 };
  
 
-//load favorites
-function loadFavoriteMovie() {
-  //gets item from local storage and saves to an array
-  favoriteMovieArr = JSON.parse(localStorage.getItem("savedMovies"));
-  console.log(favoriteMovieArr)
-  if (localStorage.getItem('savedMovies') != null){
-    displayFavoriteMovies();
-  }
-}
+// //load favorites
+// function loadFavoriteMovie() {
+//   //gets item from local storage and saves to an array
+//   favoriteMovieArr = JSON.parse(localStorage.getItem("savedMovies"));
+//   console.log(favoriteMovieArr)
+//   // if (localStorage.getItem('savedMovies') != null){
+//   //   displayFavoriteMovies();
+//   // }
+// }
 
-function displayFavoriteMovies() {
- for (var i=0; i < favoriteMovieArr.length; i++) {
-   var favMovieTitle = favoriteMovieArr[i].title
-   console.log(favMovieTitle)
-   //creates element
-    const favMovieTitleDisplay = document.createElement('h3')
-   //grab from local storage
-   favMovieTitleDisplay.innerHTML = favMovieTitle
-    favMovieEl.appendChild(favMovieTitleDisplay)
-  // var favMovieImg = favoriteMovieArr[i]
- }
-}
+// function displayFavoriteMovies() {
+//  for (var i=0; i < favoriteMovieArr.length; i++) {
+//    var favMovieTitle = favoriteMovieArr[i].title
+//    console.log(favMovieTitle)
+// // $('#movie-favorites').append(localStorage.getItem(favoriteMovieArr[i].title))
+//    //creates element
+//     const favMovieTitleDisplay = document.createElement('h3')
+//    //grab from local storage
+//    favMovieTitleDisplay.innerHTML = favMovieTitle
+//     favMovieEl.appendChild(favMovieTitleDisplay)
+//   // var favMovieImg = favoriteMovieArr[i]
+//  }
+// }
 
 
 fetchMovie();
